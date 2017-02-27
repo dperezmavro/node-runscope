19309a4827b1adbf118d5f8464c9f312c5d6ce3c
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2017-02-27 11:27:54 +0000

    added test class

--- clients/all.js
+++ clients/all.js
@@ -1,5 +1,6 @@
 module.exports = {
     Account: require('./account.js'),
     Bucket: require('./bucket.js'),
-    Team: require('./team.js')
+    Team: require('./team.js'),
+    Test: require('./test.js')
 }
---
+++ clients/test.js
@@ -0,0 +1,36 @@
+var Runscope = require('../lib/runscope.js');
+
+class Test extends Runscope {
+    constructor(token, bucket_key, test_id){
+        super(token);
+        this.bucketKey = bucket_key;
+        this.testId = test_id;
+        this.bucketName = undefined;
+        this.data = undefined;
+        this.testList = undefined;
+    }
+
+    generateTestDetailUrl(){
+        return `/buckets/${this.bucketKey}/tests/${this.testId}`;
+    }
+
+    /**
+    *   @see https://www.runscope.com/docs/api/tests#detail
+    */
+    testDetail(){
+        return new Promise((acc, rej) => {
+            this.get(this.generateTestDetailUrl())
+            .then(
+                (data) => {
+                    this.data = data.data;
+                    acc(data.data);
+                },
+                (err) => {
+                    rej(err);
+                }
+            );
+        });
+    }
+}
+
+module.exports = Test;
